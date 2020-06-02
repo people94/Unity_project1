90ddf22d89164360cbab293cd576200add07b1b7
Seung Yoon Baek <bsyu0804@naver.com>
2020-06-02 14:24:47 +0900

    총알 클래스 생성
    - 플레이어 총알발사
    - 총알프리팹 만들기
    - 플레이어 따라다니는 비행체
    - 플레이어 보조비행기 생성
    - 보조비행기 총알자동발사

---
+++ Unity Project/Assets/_BSY/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //총알클래스 하는일
+    //플레이어가 발사 버튼을 누르면
+    //총알이 생성된 후 발사하고 싶은 방향위로만 움직인다.
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    //카메라 화면밖으로나가서 보이지 않게 되면
+    //호출되는 이벤트 함수
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Unity Project/Assets/_BSY/Scripts/PlayerFire.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{    
+    public GameObject bulletFactory;//불릿 프리팹 
+    public GameObject firePoint;    //발사위치
+    public GameObject firePoint1;    //발사위치
+    public GameObject firePoint2;    //발사위치
+    private Coroutine coroutine;    //코루틴
+    private bool coru = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fire();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            transform.GetChild(1).transform.gameObject.SetActive(true);
+            StartCoroutine(corutineT1());
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            transform.GetChild(2).transform.gameObject.SetActive(true);
+            StartCoroutine(corutineT2());
+        }
+        
+    }
+
+    private void Fire()
+    {
+        //마우스왼쪽버튼 or 왼쪽컨트롤 키
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //총알공장(총알프리팹)에서 총알을 무한대로 찍어낼 수 있다.
+            //Instantiate() 함수로 프리팹 파일을 게임오브젝트로 만든다.
+
+            //총알 게임오브젝트 생성
+            GameObject bullet = Instantiate(bulletFactory);
+            //총알 오브젝트의 위치 지정
+            bullet.transform.position = firePoint.transform.position;
+        }
+    }   
+
+    IEnumerator corutineT1()
+    {
+        yield return new WaitForSeconds(1.0f);
+        
+        
+        WeaponFire1();
+        
+    }
+
+    private void WeaponFire1()
+    {
+         //총알 게임오브젝트 생성
+         GameObject bullet = Instantiate(bulletFactory);
+         //총알 오브젝트의 위치 지정
+         bullet.transform.position = firePoint1.transform.position;
+    }
+
+    IEnumerator corutineT2()
+    {
+        yield return new WaitForSeconds(1.0f);
+        
+        WeaponFire2();
+        
+    }
+
+    private void WeaponFire2()
+    {
+        //총알 게임오브젝트 생성
+        GameObject bullet = Instantiate(bulletFactory);
+        //총알 오브젝트의 위치 지정
+        bullet.transform.position = firePoint2.transform.position;
+    }
+}
