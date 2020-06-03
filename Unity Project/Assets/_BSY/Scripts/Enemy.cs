2ffd9ad59d9cfa0c38a6825d547003b37b827cfd
Seung Yoon Baek <bsyu0804@naver.com>
2020-06-03 11:38:57 +0900

    에너미 클래스 및 에너미 매니저 생성
    - 에너미 프리팹
    - 충돌처리 (에너미/플레이어, 플레이어 총알)
    - 에너미 매니저를 통한 자동 생성

---
+++ Unity Project/Assets/_BSY/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //위에서 아래로 떨어지기만 한다.(똥피하기 느낌)
+    //충돌처리(에너미랑 플레이어, 플레이어 총알과 에너미)
+
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //자기자신도 없애고
+        //충돌된 오브젝트도 없앤다.
+        //Destroy(gameObject, 1.0f); //1초 후에 없앤다.
+        Destroy(gameObject);//소문자 gameObject는 자기자신
+        Destroy(collision.gameObject);//collision은 충돌된 게임오브젝트
+    }
+}
